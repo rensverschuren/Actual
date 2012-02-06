e55db3e2a2282daab3c5f7cb71571fbbc36a7bd9
Rens <rens@imacrens.local>
2012-02-06 10:37:37 +0100

    Initial Commit

---
+++ Actual/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Actual
+//
+//  Created by Rens Verschuren on 06-02-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
